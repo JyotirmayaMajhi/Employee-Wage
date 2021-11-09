9d8980908a873538d9ce2bcc7901362103e91013
unknown <jyotirmayamajhi1998@gmail.com>
2021-11-09 16:57:45 +0530

    Added class method to compute employee wage

---
+++ src/com/bridgelabz/employeewage/Employeewageuc7.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.employeewage;
+
+public class Employeewageuc7 {
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int IS_RATE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
+	public static final int MAX_HR_IN_MONTH=10;
+	
+	public static int computeEmpWage() {
+		
+		int empHrs=0,totalempHrs=0,totalWorkingdays=0;
+		while(totalempHrs <= MAX_HR_IN_MONTH && totalWorkingdays < NUM_OF_WORKING_DAYS )
+		{
+			totalWorkingdays++;
+			int empSwitchCheck= (int)Math.floor(Math.random()*10)%3;
+			switch(empSwitchCheck) 
+			{
+			case 1:
+				empHrs=4;
+				break;
+			case 2:
+				empHrs=8;
+				break;
+			default:
+				empHrs=0;
+			}
+			totalempHrs += empHrs;
+			System.out.println("Days= " +totalWorkingdays+ "Employee Hours:" +empHrs);
+
+		}
+		int totalEmpWage = totalempHrs+IS_RATE_PER_HR;
+		System.out.println("Total Employee Wage=" +totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		computeEmpWage();
+
+	}
+
+}
